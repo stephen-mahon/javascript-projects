84c18b3bd8aa6400903c0113081a3cfc0e65e2d3
Stephen Mahon <stemahon@gmail.com>
2022-03-30 23:27:52 -0500

    updated

--- color-flipper/sketch.js
+++ color-flipper/sketch.js
@@ -1,12 +1,13 @@
-function setup() {
-  createCanvas(400, 400);
-  var button = createButton("CLICK ME");
-  let a = button.mousePressed(colorFlipper);
-}
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
 
-function colorFlipper() {
-  let r = floor(random(0, 255));
-  let g = floor(random(0, 255));
-  let b = floor(random(0, 255));
-  background(r, g, b);
+btn.addEventListener("click", function(){
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random()*colors.length);
 }
